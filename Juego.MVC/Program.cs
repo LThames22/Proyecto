f7f43a94b7a9cbb6f329a3bfb4feb6b3e7e236ef
LThames22 <likrein22@gmail.com>
2021-12-01 15:07:43 -0300

    asd

---
+++ Juego.MVC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Juego.MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
