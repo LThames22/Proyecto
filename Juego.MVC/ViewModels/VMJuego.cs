874f580402da868d2a619aba60fa05e2938baf87
LThames22 <likrein22@gmail.com>
2021-12-01 15:12:13 -0300

    asd

---
+++ Juego.MVC/ViewModels/VMJuego.cs
@@ -0,0 +1,20 @@
+﻿using Jueg.Core;
+using System.Collections.Generic;
+
+namespace Juego.MVC.ViewModels
+{
+    public class VMJuego
+    {
+        public IEnumerable<Genero> Generos { get; set; }
+        public Juego Juego { get; set; }
+        public int? IdGeneroSeleccionado { get; set; }
+        public VMJuego() => Juego = new Juego();
+        public VMJuego(IEnumerable<Genero> generos) : this()
+            => Generos = generos;
+
+        public VMJuego(Juego juego)
+        {
+            Juego = juego;
+        }
+    }
+}
